4dcec9c7db961a4af6299fb344d1128b576a3c39
ramillamamedova <114265678+ramillamamedova@users.noreply.github.com>
2024-12-08 01:05:19 +0200

    Created empty project
    absolutely empty/

---
+++ HomeWork5/HomeWork5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork5
+//
+//  Created by Ram on 08.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
